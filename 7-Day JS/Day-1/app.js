181f7430e9d1f815910ac6b42708d0911e1d6efc
sharozijaz <sharozijaz4@gmail.com>
2021-05-21 22:03:48 +0500

    Day 1 Complete of 7-Day JS

---
+++ 7-Day JS/Day-1/app.js
@@ -0,0 +1,46 @@
+// Challenge 1
+/* 
+const markMass = 78;
+const markHeight = 1.69;
+const johnHeight = 1.95;
+const johnMass = 85;
+let markBMI = markMass / markHeight ** 2;
+let johnBMI = johnMass / johnHeight ** 2;
+
+const whosHiegherBMI = markBMI > johnBMI;
+console.log(whosHiegherBMI);
+console.log(markBMI, johnBMI);
+*/
+
+// Template Literals
+/*
+const name = 'sharoz ijaz';
+const age = 21;
+const job = 'web develper';
+const laptop= 'dell';
+
+const bio = `my name ${name} and i am ${age} old, i am a ${job}, i use ${laptop} laptop to work.`;
+
+console.log(bio);
+*/
+
+// If else statments
+/* 
+const userName = 'Sharoz Ijaz';
+const age = 31;
+
+if (userName == 'Sharoz Ijaz' && age >= 21){
+    console.log(`Welcome to web programming Mr ${userName},`);
+}else{
+    console.log(`Sorry Mr ${userName} you are not Allowed`);
+}
+*/
+// let bookQty = prompt('hellos');
+// const books = 200;
+
+// if ( parseInt(bookQty)  >= books ){
+//     console.log(`Sorry we don't have that much of books, we only have ${parseInt(bookQty) - books} left. `);
+// }
+// else{
+//     console.log(`Good your order is on the way,we only have ${books - parseInt(bookQty)} left.`);
+// };
